9ff60d42ba8d7abe10f713d23aaaf1ab466bd2c4
yeti-s <sel9371@gmail.com>
2024-02-21 22:02:35 +0900

    feat: create manifest with multi threads

--- data/ko_dialogue.py
+++ data/ko_dialogue.py
@@ -7,13 +7,15 @@ import argparse
 from tqdm import tqdm
 from glob import glob
 from enum import Enum
-from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 from g2pk import G2p
 from lhotse.audio.recording import Recording
 from lhotse.audio.recording_set import RecordingSet
 from lhotse.supervision import SupervisionSegment, SupervisionSet
 
+NUM_THREADS = multiprocessing.cpu_count()
+DIR_PREFIX = os.path.join('data', 'remote', 'PROJECT', 'AI학습데이터', 'KoreanSpeech', 'data')
 
 class SexCode(Enum):
     MALE = 'M'
@@ -48,9 +50,6 @@ class QualityCode(Enum):
     REMOTE = 4
     
 
-G2P = G2p()
-DIR_PREFIX = os.path.join('data', 'remote', 'PROJECT', 'AI학습데이터', 'KoreanSpeech', 'data')
-
 def set_root_path(root_path):
     global DIR_PREFIX
     DIR_PREFIX = os.path.join(root_path, DIR_PREFIX)
@@ -70,7 +69,7 @@ def unzip_all(root_dir):
 
 
 class MetaData():
-    def __init__(self, data, filter=None) -> None:
+    def __init__(self, data, g2p, filter=None) -> None:
         self.valid = False
         
         self.file_path = os.path.join(DIR_PREFIX, data[0][1:])
@@ -110,7 +109,7 @@ class MetaData():
             return
         
         with open(text_path, 'r', encoding='utf-8') as f:
-            self.text = G2P(f.read())
+            self.text = g2p(f.read())
         
         self.valid = True
         
@@ -136,42 +135,104 @@ def read_meta_data(file_path, filter):
 
 
 def get_metadata_list(root_path, filter=None):
+    print(f'=== find *_metadata.txt in {root_path}')
     metadata_files = glob(os.path.join(root_path, '**', '*_metadata.txt'), recursive=True)
+    num_metadata_files = len(metadata_files)
+    print(f'-- {num_metadata_files} files found.')
+
+    # define task
+    def task(lines):
+        g2p = G2p()
+        metadata_sub_list = []
+        
+        for line in lines:
+            metadata = MetaData(line.strip().split(' | '), g2p, filter)
+            if metadata.valid:
+                metadata_sub_list.append(metadata)
+                
+        return metadata_sub_list
+        
     metadata_list = []
-    
-    # using thread
-    task = lambda metadata_file: metadata_list.extend(read_meta_data(metadata_file, filter))
-    
-    thread_count = multiprocessing.cpu_count()
-    with ThreadPoolExecutor(max_workers=thread_count) as executor:
-        executor.map(task, metadata_files)
+
+    for i in tqdm(range(num_metadata_files)):
+        metadata_file = metadata_files[i]
+        
+        with open(metadata_file, 'r', encoding='utf-8') as file:
+            lines = file.readlines()
+            num_lines = len(lines)
+            
+            # process with multi thread
+            if NUM_THREADS > 1:
+                unit_tasks = num_lines // NUM_THREADS
+                with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+                    futures = [executor.submit(task, lines[unit_tasks*t:unit_tasks*(t+1)]) for t in range(NUM_THREADS-1)]
+                    futures.append(executor.submit(task, lines[unit_tasks*(NUM_THREADS-1):]))
+
+                    for future in as_completed(futures):
+                        metadata_list.extend(future.result())
+            else:
+                metadata_list = task(lines)
+        
+    print(f'-- {len(metadata_list)} data created.')
     
     return metadata_list
 
 
 
 def create_manifest(metadata_list, dest):
-    recordings = RecordingSet.from_recordings(
-        Recording.from_file(metadata.file_path, recording_id=metadata.id) for metadata in metadata_list
-    )
-    supervisions = SupervisionSet.from_segments(
-        SupervisionSegment(
-            id=metadata.id,
-            recording_id=metadata.id,
+    print(f'=== create manifest from {len(metadata_list)} metadata')
+    
+    num_data = len(metadata_list)
+    # create recordings & supervisions
+    recordings = []
+    supervisions = []
+    print(f'-- create recordings')
+    
+    def task(metadata_sub_list):
+        sub_recordings = [Recording.from_file(
+            metadata.file_path, 
+            recording_id=metadata.file_path
+        ) for metadata in metadata_sub_list]
+        
+        sub_supervisions = [SupervisionSegment(
+            id=metadata.file_path,
+            recording_id=metadata.file_path,
             start=metadata.start,
-            duration=recordings[metadata.id].duration,
+            duration=metadata.end,
             text=metadata.text,
-            language='Korean'
-        ) for metadata in metadata_list
-    )
+            language='Korean',
+            custom={'normalized_text': metadata.text.strip()}
+        ) for metadata in metadata_sub_list]
+        
+        return sub_recordings, sub_supervisions
+        
+    # process with multi threads
+    if NUM_THREADS > 1:
+        unit_tasks = num_data // NUM_THREADS
+        with ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+            futures = [executor.submit(task, metadata_list[unit_tasks*t:unit_tasks*(t+1)]) for t in range(NUM_THREADS-1)]
+            futures.append(executor.submit(task, metadata_list[unit_tasks*(NUM_THREADS-1):]))
 
-    # Save manifests
-    recordings.to_jsonl(os.path.join(dest, 'ljspeech_recordings_all.jsonl.gz'))
-    supervisions.to_jsonl(os.path.join(dest, 'ljspeech_supervisions_all.jsonl.gz'))
+            for future in as_completed(futures):
+                sub_recordings, sub_supervisions = future.result()
+                recordings.extend(sub_recordings)
+                supervisions.extend(sub_supervisions)
+    # process with single thread
+    else:
+        recordings, supervisions = task(metadata_list)        
+    
+    # create recording set & supervision set
+    recording_set = RecordingSet.from_recordings(recordings)
+    supervision_set = SupervisionSet.from_segments(supervisions)
+    # export data
+    recording_set.to_jsonl(os.path.join(dest, 'ljspeech_recordings_all.jsonl.gz'))
+    supervision_set.to_jsonl(os.path.join(dest, 'ljspeech_supervisions_all.jsonl.gz'))
+    print(f'-- recordings & supervisions is saved on {dest}')
 
 
 def main(args):
     data_path = args.data
+    out_path = args.out
     set_root_path(data_path)
 
     # unzip
@@ -186,14 +247,13 @@ def main(args):
         filter['age'] = args.age
  
     metadata_list = get_metadata_list(data_path, filter)
-    print(f'{len(metadata_list)} data imported.')
-
-    create_manifest(metadata_list, data_path)
+    create_manifest(metadata_list, out_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data', required=True, help='korean dialouge data path')
+    parser.add_argument('--data', required=True, help='korean dialouge data path (required!)')
     parser.add_argument('--unzip', action='store_const', const=True, default=False, help='unzip .tar.gz files')
     parser.add_argument('--sex', help='M or F')
     parser.add_argument('--age', type=list, help='["C"(child), "T"(teenager), "A"(adult), "S"(senior), "F"(etc)]')
+    parser.add_argument('--out', default='data/manifests', help='output directory. default is "data/manifests"')
     main(parser.parse_args())
